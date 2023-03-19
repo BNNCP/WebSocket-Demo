ce329af5fc620ab000ab9e9e645a365b105e3f8d
z7016187 <asd880810@gmail.com>
2023-03-20 01:17:16 +0800

    websocket-demo

---
+++ WebSocket_ASP/Controllers/WebSocketController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+public class WebSocketController : Controller
+{
+    public static List<System.Net.WebSockets.WebSocket> connections = new List<System.Net.WebSockets.WebSocket>();
+    WebSocketServer wssv = new WebSocketServer("ws://127.0.0.1:7890");
+    [HttpGet("/ws")]
+    public async Task Get()
+    {
+        //是否為webSocket請求 如果是則加入等待
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+            PlayerRef.id = generateID();
+            connections.Add(webSocket);
+            Console.WriteLine("New Client Connected");
+            await Echo(webSocket);
+        }
+        else
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+
+    private static async Task Echo(System.Net.WebSockets.WebSocket webSocket)
+    {
+        var buffer = new byte[1024 * 4];
+        //等待接收訊息
+        var receiveResult = await webSocket.ReceiveAsync(
+             new ArraySegment<byte>(buffer), CancellationToken.None);
+        //檢查是否為連線狀態
+
+
+        while (!receiveResult.CloseStatus.HasValue)
+        {
+            //訊息發到前端
+
+            for (int i = 0; i < connections.Count; i++)
+            {
+                if (connections[i].State != System.Net.WebSockets.WebSocketState.Open)
+                {
+                    continue;
+                }
+                await connections[i].SendAsync(
+               new ArraySegment<byte>(buffer, 0, receiveResult.Count),
+               receiveResult.MessageType,
+               receiveResult.EndOfMessage,
+               CancellationToken.None);
+            }
+
+            //繼續等待接收訊息
+            receiveResult = await webSocket.ReceiveAsync(
+                new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        }
+        //關閉連線
+
+        await webSocket.CloseAsync(
+            receiveResult.CloseStatus.Value,
+            receiveResult.CloseStatusDescription, CancellationToken.None);
+
+
+    }
+    public string generateID()
+    {
+        return Guid.NewGuid().ToString("N");
+    }
+}
+
---
+++ WebSocket_ASP/Models/PlayerRef.cs
@@ -0,0 +1,10 @@
+public static class PlayerRef{
+    public static string id { get; set; }
+    public static string name { get; set; }
+    public static string direction { get; set; }
+    public static string color { get; set; }
+    public static int x { get; set; }
+    public static int y { get; set; }
+    public static int coins { get; set; }
+
+}
